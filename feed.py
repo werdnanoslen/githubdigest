e05f8c27672fecbb17173a342108e7df4eedc4e2
Andrew Nelson <andy@andyhub.com>
2015-06-03 23:46:42 -0700

    added bitly shortlinking

--- feed.py
+++ feed.py
@@ -1,7 +1,12 @@
 import feedparser
+import requests
+import urllib
 from datetime import datetime
+from twitter import *
 
 feed = feedparser.parse('https://github.com/werdnanoslen.atom')
+bitlyToken = ''
+bitlyApi = 'https://api-ssl.bitly.com/v3/shorten?access_token=' + bitlyToken + '&longUrl='
 
 dateformat = '%Y-%m-%dT%H:%M:%S'
 now = datetime.strptime(datetime.utcnow().strftime(dateformat), dateformat)
@@ -12,12 +17,22 @@ for i in range(len(feed.entries)):
     then = datetime.strptime(entry.published.replace('Z', ''), dateformat)
     daysSince = (now - then).days
     if daysSince == 0:
-        eventsYesterday.append(entry.title)
+        bitlyResponse = requests.get(bitlyApi + entry.link).json()
+        if bitlyResponse['status_txt'] == "OK":
+            entryTitle = entry.title.replace('werdnanoslen ', '')
+            bitlyLink = bitlyResponse['data']['url'].replace('http://', '')
+            event = entryTitle + ' ' + bitlyLink
+            eventsYesterday.append(event)
+        else:
+            print('bitly api error: ' + bitlyResponse.status_txt)
     else:
         break
 
 numEventsYesterday = len(eventsYesterday)
+tweetContent = ''
 if numEventsYesterday == 1:
-    print('I did a thing')
+    tweetContent = eventsYesterday
 elif numEventsYesterday > 1:
-    print('I did some things')
+    tweetContent = eventsYesterday
+
+print(tweetContent)
